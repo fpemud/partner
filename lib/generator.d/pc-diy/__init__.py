1d2e867e27ff8053747b8453b0da0eda31052568
fpemud <fpemud@sina.com>
2021-01-19 03:21:21 +0000

    fix

---
+++ lib/generator.d/pc-diy/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# -*- coding: utf-8; tab-width: 4; indent-tabs-mode: t -*-
+
+class KgGenerator:
+
+    def __init__(self, conn):
+        self._conn = conn
+
+    def generate(self):
+        pass
+
